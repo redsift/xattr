d2b2a9124120690b86df84c42f883c04569dd564
Rahul Powar <rahul@redsift.io>
2015-10-15 18:32:14 +0100

    Ported fix to darwin

--- syscall_darwin.go
+++ syscall_darwin.go
@@ -6,13 +6,28 @@ import (
 )
 
 func getxattr(path string, name string, value *byte, size int, pos int, options int) (int, error) {
-	r0, _, e1 := syscall.Syscall6(syscall.SYS_GETXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(unsafe.Pointer(value)), uintptr(size), uintptr(pos), uintptr(options))
-	return int(r0), e1
+	if r0, _, e1 := syscall.Syscall6(syscall.SYS_GETXATTR,
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(name))),
+		uintptr(unsafe.Pointer(value)), uintptr(size), uintptr(pos), uintptr(options)); e1 != syscall.Errno(0) {
+		if e1 == syscall.ENODATA {
+			return 0, e1
+		}
+		return -1, e1
+	} else {
+		return int(r0), nil
+	}
 }
 
 func listxattr(path string, namebuf *byte, size int, options int) (int, error) {
-	r0, _, e1 := syscall.Syscall6(syscall.SYS_LISTXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(namebuf)), uintptr(size), uintptr(options), 0, 0)
-	return int(r0), e1
+	if r0, _, e1 := syscall.Syscall6(syscall.SYS_LISTXATTR,
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+		uintptr(unsafe.Pointer(namebuf)),
+		uintptr(size), uintptr(options), 0, 0); e1 != syscall.Errno(0) {
+		return -1, e1
+	} else {
+		return int(r0), nil
+	}
 }
 
 func setxattr(path string, name string, value *byte, size int, pos int, options int) error {
--- syscall_linux.go
+++ syscall_linux.go
@@ -7,34 +7,34 @@ import (
 
 func getxattr(path string, name string, value *byte, size int) (int, error) {
 	if r0, _, e1 := syscall.Syscall6(syscall.SYS_GETXATTR,
-						 uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
-						 uintptr(unsafe.Pointer(syscall.StringBytePtr(name))),
-						 uintptr(unsafe.Pointer(value)), uintptr(size), 0, 0); e1 != syscall.Errno(0){
-	if e1 == syscall.ENODATA {
-		return 0, e1 
-	}
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(name))),
+		uintptr(unsafe.Pointer(value)), uintptr(size), 0, 0); e1 != syscall.Errno(0) {
+		if e1 == syscall.ENODATA {
+			return 0, e1
+		}
 		return -1, e1
-	} else{
+	} else {
 		return int(r0), nil
 	}
 }
 
 func listxattr(path string, namebuf *byte, size int) (int, error) {
 	if r0, _, e1 := syscall.Syscall(syscall.SYS_LISTXATTR,
-						 uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
-						 uintptr(unsafe.Pointer(namebuf)),
-						 uintptr(size)); e1 != syscall.Errno(0){
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+		uintptr(unsafe.Pointer(namebuf)),
+		uintptr(size)); e1 != syscall.Errno(0) {
 		return -1, e1
 	} else {
 		return int(r0), nil
-	}	
+	}
 }
 
 func setxattr(path string, name string, value *byte, size int) (err error) {
 	_, _, e1 := syscall.Syscall6(syscall.SYS_SETXATTR,
-				 uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
-	 			 uintptr(unsafe.Pointer(syscall.StringBytePtr(name))),
-				 uintptr(unsafe.Pointer(value)), uintptr(size), 0, 0)
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+		uintptr(unsafe.Pointer(syscall.StringBytePtr(name))),
+		uintptr(unsafe.Pointer(value)), uintptr(size), 0, 0)
 	return e1
 }
 
